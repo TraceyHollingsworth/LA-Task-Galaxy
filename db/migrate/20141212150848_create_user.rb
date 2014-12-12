f1f2304aa20c3261f6704714c1322bd0c5adda85
TraceyHollingsworth <tracey.hollingsworth@gmail.com>
2014-12-12 10:14:58 -0500

    created user model and migration

--- db/migrate/20141212150848_create_user.rb
+++ db/migrate/20141212150848_create_user.rb
@@ -1,4 +1,15 @@
 class CreateUser < ActiveRecord::Migration
   def change
+    create_table :users do |table|
+      table.string :email, null: false
+      table.string :password, null: false
+      table.string :first_name
+      table.string :last_name
+
+      table.timestamps
+    end
+
+    add_index :users, [:email], unique: true
   end
 end
+
